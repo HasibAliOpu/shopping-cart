88f68db79173c6e8b1f03a54f61ed125543c4a59
H_A _Opu <wwwhasib4@gmail.com>
2022-02-14 15:32:08 +0600

    shopping cart

---
+++ app.js
@@ -0,0 +1,56 @@
+function updateProductNumber(product,price,isIncreasing){
+    const productInput = document.getElementById(product+'-number');
+    let productNumber = productInput.value;
+    if(isIncreasing == true){
+        productNumber = parseInt(productNumber) + 1;
+    }
+    else if(productNumber > 0){
+        productNumber = parseInt(productNumber) - 1;
+    }
+    
+    productInput.value = productNumber;
+    // update total
+    const totalproduct = document.getElementById(product+'-total');
+    totalproduct.innerText = productNumber * price;
+    calculateTotal();
+}
+function getInputValue(product){
+    const productInput = document.getElementById(product+'-number');{
+        const productNumber = parseInt(productInput.value);
+        return productNumber;
+    }
+}
+function calculateTotal(){
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    // update on sub-Total
+    /* const totalAmount = document.getElementById('sub-total');
+    const previousAmount = parseInt(totalAmount.innerText);
+    totalAmount.innerText = subTotal;*/
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
+}
+    // phone increase discrease events
+document.getElementById('phone-plus').addEventListener('click',function(){
+    updateProductNumber('phone',1219,true)
+})
+document.getElementById('phone-minus').addEventListener('click',function(){
+    updateProductNumber('phone',1219,false)
+})
+    // case increase discrease events
+document.getElementById('case-plus').addEventListener('click',function(){
+ /*    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;  */
+    updateProductNumber('case',59,true); 
+})
+document.getElementById('case-minus').addEventListener('click',function(){
+   /*  const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) -1; */
+    updateProductNumber('case',59,false);
+})
